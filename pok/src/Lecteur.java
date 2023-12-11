3d3ad8d2e6a79a800299baa15627143a2e9aefcb
maceo <mrenault@et.esiea.fr>
2023-12-11 16:12:52 +0100

    Debut dev class Lecture et rawMonster

--- pok/src/Lecteur.java
+++ pok/src/Lecteur.java
@@ -1,2 +1,78 @@
-public class Lecteur {
+import java.util.List;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Lecteur
+{
+    private List<rawMonster> listRawMonster;
+
+// ====================================================
+    public List<rawMonster> getListRawMonster(String _filePath)
+    {
+        this.listRawMonster = readMonstersFromFile(_filePath);
+        return this.listRawMonster;
+    }
+
+    private static List<rawMonster> readMonstersFromFile(String _filePath)
+    {
+        List<rawMonster> listMonsters = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(_filePath)))
+        {
+            String line;
+            boolean isReadingMonster = false;
+            rawMonster currentMonster = null;
+
+            while ((line = br.readLine()) != null)
+            {
+                if (line.startsWith("Monster"))
+                {
+                    isReadingMonster = true;
+                    currentMonster = new rawMonster();
+                } else if (line.startsWith("EndMonster"))
+                {
+                    isReadingMonster = false;
+                    if (currentMonster != null)
+                    {
+                        listMonsters.add(currentMonster);
+                    }
+                } else if (isReadingMonster)
+                {
+                    String[] parts = line.split(" ");
+                    if (parts.length >= 2)
+                    {
+                        String attribute = parts[0];
+                        String value = parts[1];
+
+                        switch (attribute)
+                        {
+                            case "Name":
+                                currentMonster.setName(value);
+                                break;
+                            case "Type":
+                                currentMonster.setType(value);
+                                break;
+                            case "HP":
+                                String[] hpValues = value.split(" ");
+                                if (hpValues.length == 2) {
+                                    currentMonster.setMinHP(Integer.parseInt(hpValues[0]));
+                                    currentMonster.setMaxHP(Integer.parseInt(hpValues[1]));
+                                }
+                                break;
+                            // Handle other attributes similarly
+                            default:
+                                // Handle unrecognized attributes
+                                break;
+                        }
+                    }
+                }
+            }
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return listMonsters;
+    }
 }
---
+++ pok/src/rawMonster.java
@@ -0,0 +1,103 @@
+public class rawMonster
+{
+    private String name;
+    private String type;
+    private int minHP;
+    private int maxHP;
+    private int minSpeed;
+    private int maxSpeed;
+    private int minAttack;
+    private int maxAttack;
+    private int minDefense;
+    private int maxDefense;
+    private double paralysisChance;
+    //==========================================================================
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getMinHP() {
+        return minHP;
+    }
+
+    public void setMinHP(int minHP) {
+        this.minHP = minHP;
+    }
+
+    public int getMaxHP() {
+        return maxHP;
+    }
+
+    public void setMaxHP(int maxHP) {
+        this.maxHP = maxHP;
+    }
+
+    public int getMinSpeed() {
+        return minSpeed;
+    }
+
+    public void setMinSpeed(int minSpeed) {
+        this.minSpeed = minSpeed;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getMinAttack() {
+        return minAttack;
+    }
+
+    public void setMinAttack(int minAttack) {
+        this.minAttack = minAttack;
+    }
+
+    public int getMaxAttack() {
+        return maxAttack;
+    }
+
+    public void setMaxAttack(int maxAttack) {
+        this.maxAttack = maxAttack;
+    }
+
+    public int getMinDefense() {
+        return minDefense;
+    }
+
+    public void setMinDefense(int minDefense) {
+        this.minDefense = minDefense;
+    }
+
+    public int getMaxDefense() {
+        return maxDefense;
+    }
+
+    public void setMaxDefense(int maxDefense) {
+        this.maxDefense = maxDefense;
+    }
+
+    public double getParalysisChance() {
+        return paralysisChance;
+    }
+
+    public void setParalysisChance(double paralysisChance) {
+        this.paralysisChance = paralysisChance;
+    }
+}
